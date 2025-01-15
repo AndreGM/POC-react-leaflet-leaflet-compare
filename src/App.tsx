fb9eeb139a165fae0817c0b0411a47cf7cff7689
Andre Gomes <andre@appdev.com.br>
2025-01-15 11:53:49 -0300

    Refator with wms sources

--- src/App.tsx
+++ src/App.tsx
@@ -6,38 +6,66 @@ import "leaflet-compare/dist/leaflet-compare";
 import { useEffect, useRef, useState } from "react";
 import L from "leaflet";
 
-function LeafletCompare({ show } : { show: boolean }) {
+function LeafletCompare({ show }: { show: boolean }) {
   const map = useMap();
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const compareRef = useRef<any>(null);
 
   useEffect(() => {
-    if (compareRef.current && compareRef.current !== undefined && compareRef.current !== null) {
+    if (
+      compareRef.current &&
+      compareRef.current !== undefined &&
+      compareRef.current !== null
+    ) {
       compareRef.current.remove();
       compareRef.current = null;
     }
-
+    
     if (show) {
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
       map.createPane("left");
       map.createPane("right");
 
-   
+      const wmsLayer1 = L.tileLayer.wms(
+        "https://datageo.ambiente.sp.gov.br/geoserver/datageo/wms?",
+        {
+          layers: "datageo:IGR2024",
+          format: "image/png",
+          transparent: true,
+          attribution: "DataGEO",
+          pane: "left",
+        }
+      );
 
-      const stamenLayer = L.tileLayer('https://tileserver.memomaps.de/tilegen/{z}/{x}/{y}.png', {
-        maxZoom: 18,
-        attribution: 'Map <a href="https://memomaps.de/">memomaps.de</a> <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        pane: "right"
-      });
+      const wmsLayer2 = L.tileLayer.wms(
+        "https://datageo.ambiente.sp.gov.br/geoserver/datageo/wms?",
+        {
+          layers: "datageo:IGR2023",
+          format: "image/png",
+          transparent: true,
+          attribution: "DataGEO",
+          pane: "right",
+        }
+      );
 
-      const osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-        pane: "left"
-      });
-      
-      stamenLayer.addTo(map);
-      osmLayer.addTo(map);
+      wmsLayer1.addTo(map);
+      wmsLayer2.addTo(map);
+
       // @ts-expect-error compare is not part of the types
-      compareRef.current = L.control.compare([stamenLayer], [osmLayer]).addTo(map);
+      compareRef.current = L.control.compare([wmsLayer1], [wmsLayer2]).addTo(map);
+    }
+    else{
+      map.eachLayer((layer) => {
+        if (layer.options.pane === "left" || layer.options.pane === "right") {
+          map.removeLayer(layer);
+
+    }
+  });
     }
   }, [map, show]);
 
@@ -49,11 +77,16 @@ function App() {
 
   return (
     <div style={{ height: "100vh", width: "100%" }}>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} dragging={false} style={{ height: "100%", width: "100%" }}>
-        
+      <MapContainer
+        center={[-23, -46]}
+        zoom={7}
+        scrollWheelZoom={false}
+        dragging={false}
+        style={{ height: "100%", width: "100%" }}
+      >
         <LeafletCompare key={"compare"} show={showCompare} />
-        <button 
-          style={{ position: 'absolute', top: 10, right: 10, zIndex: 1000 }} 
+        <button
+          style={{ position: "absolute", top: 10, right: 10, zIndex: 1000 }}
           onClick={() => setShowCompare(!showCompare)}
         >
           {showCompare ? "Esconder" : "Mostrar"} Comparador
